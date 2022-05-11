c51f0d72f449affbb0fd7bb278d6873562656ac0
Alconite <Alconite@HOME-PC>
2022-05-12 00:59:33 +0200

    Pociąg jeżdzi

--- SO_Projekt/Form1.Designer.cs
+++ SO_Projekt/Form1.Designer.cs
@@ -29,21 +29,42 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.train = new System.Windows.Forms.PictureBox();
+            this.szlaban_1 = new System.Windows.Forms.PictureBox();
+            this.szlaban_2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.train)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.szlaban_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.szlaban_2)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // train
             // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(1044, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(60, 66);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Visible = false;
+            this.train.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.train.Image = ((System.Drawing.Image)(resources.GetObject("train.Image")));
+            this.train.Location = new System.Drawing.Point(1044, 0);
+            this.train.Name = "train";
+            this.train.Size = new System.Drawing.Size(60, 66);
+            this.train.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.train.TabIndex = 0;
+            this.train.TabStop = false;
+            // 
+            // szlaban_1
+            // 
+            this.szlaban_1.Location = new System.Drawing.Point(1010, 491);
+            this.szlaban_1.Name = "szlaban_1";
+            this.szlaban_1.Size = new System.Drawing.Size(26, 113);
+            this.szlaban_1.TabIndex = 1;
+            this.szlaban_1.TabStop = false;
+            this.szlaban_1.Visible = false;
+            // 
+            // szlaban_2
+            // 
+            this.szlaban_2.Location = new System.Drawing.Point(1114, 491);
+            this.szlaban_2.Name = "szlaban_2";
+            this.szlaban_2.Size = new System.Drawing.Size(26, 113);
+            this.szlaban_2.TabIndex = 2;
+            this.szlaban_2.TabStop = false;
+            this.szlaban_2.Visible = false;
             // 
             // Form1
             // 
@@ -52,18 +73,24 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1230, 651);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.szlaban_2);
+            this.Controls.Add(this.szlaban_1);
+            this.Controls.Add(this.train);
             this.DoubleBuffered = true;
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.train)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.szlaban_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.szlaban_2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox train;
+        private System.Windows.Forms.PictureBox szlaban_1;
+        private System.Windows.Forms.PictureBox szlaban_2;
     }
 }
 
--- SO_Projekt/Form1.cs
+++ SO_Projekt/Form1.cs
@@ -13,11 +13,57 @@ namespace SO_Projekt
 {
     public partial class Form1 : Form
     {
-        TrainMove tm = new TrainMove();
+        int poz_pociagu_x = 1044;
+        int poz_pociagu_y = 0;
         public Form1()
         {
             InitializeComponent();
-            Thread train = new Thread(tm.TrainMoving);
+            Thread trainMove = new Thread(TrainMove);
+            trainMove.Start();
+        }
+
+        private void TrainMove()
+        {
+            Thread.Sleep(1000);
+            while (true)
+            {
+                if (InvokeRequired)
+                {
+                    train.Invoke(new Action(delegate ()
+                    {
+                        train.Image = null;
+                        train.Refresh();
+
+                        train.Location = new Point(poz_pociagu_x, poz_pociagu_y);
+                        Thread.Sleep(10);
+                        poz_pociagu_y++;
+
+                        Szlaban();
+                        if (poz_pociagu_y == 680)
+                        {
+                            poz_pociagu_y = 0;
+                            train.Location = new Point(poz_pociagu_x, poz_pociagu_y);
+                        }
+                    }));
+                    Thread.Sleep(5);
+                }
+            }
+        }
+
+        void Szlaban()
+        {
+            szlaban_1.Image = null;
+            szlaban_2.Image = null;
+            if (poz_pociagu_y >= 300)
+            {
+                szlaban_1.Visible = true;
+                szlaban_2.Visible = true;
+            }
+            else
+            {
+                szlaban_1.Visible = false;
+                szlaban_2.Visible = false;
+            }
         }
 
       
